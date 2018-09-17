82b627ad6acee7dec5c833f2499a673939433dd3
AnayGondhalekar <anay.gondhalekar@colorado.edu>
2018-09-16 23:36:32 -0600

    Remaining name change

--- example.py
+++ example.py
@@ -25,7 +25,7 @@ __copyright__ = "Copyright (C) 2018 by Anay Gondhalekar"
 # Redistribution, modification or use of this software in source or binary
 # forms is permitted as long as the files maintain this copyright. Users are
 # permitted to modify this and use it to learn about the field of embedded
-# software. Bruce Montgomery and the University of Colorado
+# software. Anay Gondhalekar and the University of Colorado
 # are not liable for any misuse of this material.
 #
 
